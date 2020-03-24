53e77581c3e8955d29ee7872626a4f2ec4b45d4e
Nicolas Meienberger <n.meienberger@gmail.com>
2020-03-25 00:45:52 +0100

    [FIX] Export bundle

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import VisibilitySensor from 'react-visibility-sensor';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const ProgressiveImage = ({ render, uri, thumbnail, initialBlur, timingFunction, transitionTime }) => {
+	const [blur, setBlur] = useState(initialBlur);
+	const [src, setSrc] = useState(thumbnail);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	const fetch = useCallback(() => {
+		console.log('FETCH', uri);
+		const image = new Image();
+		image.src = uri;
+		image.addEventListener(
+			'load',
+			() => {
+				setSrc(uri);
+				setBlur(0);
+			},
+			false
+		);
+	}, [uri]);
+
+	const getStyle = () => {
+		return {
+			filter: `blur(${blur}px)`,
+			transition: `filter ${transitionTime}ms ${timingFunction}`,
+		};
+	};
+
+	return (
+		<VisibilitySensor partialVisibility={true}>
+			{({ isVisible }) => {
+				if (isVisible && blur) fetch();
+
+				return render(src, getStyle());
+			}}
+		</VisibilitySensor>
+	);
+};
+
+ProgressiveImage.defaultProps = {
+	transitionTime: 500,
+	timingFunction: 'ease',
+	initialBlur: 10,
+};
+
+ProgressiveImage.propTypes = {
+	transitionTime: PropTypes.number,
+	timingFunction: PropTypes.string,
+	initialBlur: PropTypes.number,
+	uri: PropTypes.string.isRequired,
+};
+
+export default ProgressiveImage;
